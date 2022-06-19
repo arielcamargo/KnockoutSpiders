877954ceba679118ef06936317048ac3e693d991
quanduong95 <60638859+quanduong95@users.noreply.github.com>
2022-06-18 18:22:41 -0700

    css for modal filters form, wrote events handler for filters form and redesign the bottom panels

--- src/dashboard/dashboard.component.ts
+++ src/dashboard/dashboard.component.ts
@@ -2,8 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: '/dashboard.component.html',
-  styleUrls: ['/dashboard.component.css', '../assets/fonts/fontawesome-free-6.1.1-web/css/all.min.css']
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css', '../assets/fonts/fontawesome-free-6.1.1-web/css/all.min.css']
 
 })
 
