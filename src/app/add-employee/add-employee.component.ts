1b0d0a0e879b3f6d9a99620f5190b5e94a8bd465
arielcamargo <70982774+arielcamargo@users.noreply.github.com>
2022-06-27 12:41:00 -0700

    You can now navigate between both pages.

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-add-employee',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+  }
+  ngOnInit()
+  {
   }
 
+  clickedAddButton()
+  {
+    //This would pull all data from
+    //this form and click it back to
+    //Vendia
+    this.goToDashboard();
+  }
+
+  goToDashboard()
+  {
+    this.router.navigate(['']);
+  }
 }
