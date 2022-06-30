ebfbcf7540bb59cee0816ae224fb1e05bcf79a1b
Hamidullahaslan <hamid_omed@yahoo.com>
2022-06-30 16:15:21 -0700

    improvement on teable-display

--- src/app/table-display/table-display.component.ts
+++ src/app/table-display/table-display.component.ts
@@ -17,23 +17,73 @@ const {entities} = client;
 @Component({
   selector: 'app-table-display',
   templateUrl: './table-display.component.html',
-  styleUrls: ['./table-display.component.css']
+  styleUrls: ['./table-display.component.css'],
 })
 export class TableDisplayComponent implements OnInit {
-
+  
   constructor(private router: Router) { }
-  ageArr: number[];
+  
+  arrayObj: object[] = [];
   test = 0;
   ngOnInit()
   {
-
+    async function customTesting() {
+      console.log('works???');
+      // @ts-ignore
+      const employeeResponse = await entities.employee.list();
+      return employeeResponse;
   }
 
-  async customTesting()
-  {
-    // @ts-ignore
-    const employeeResponse = await entities.employee.list();
-    return employeeResponse;
+  let firstName= "";
+  let lastName= "";
+  let age = 0;
+  let gender = "";
+  let weight = 0;
+  let height = 0;
+  let bloodPressure = 0;
+  let pulseRate = 0;
+  let exercisePerWeek = 0;
+  let vacationBalance = 0;
+  let workPerWeek = 0;
+  let bodyTemp = 0;
+  let respirationRate = 0;
+  customTesting().then(data => {
+    for(let i = 0;i < data.items.length;i++){
+      let obj = {
+        firstName: "",
+        lastName: "",
+        age: 0,
+        gender: "",
+        weight: 0,
+        height: 0,
+        bloodPressure: "",
+        pulseRate: 0,
+        exercisePerWeek: 0,
+        vacationBalance: 0,
+        workPerWeek: 0,
+        bodyTemp: 0,
+        respirationRate: 0,
+      }
+      obj.firstName = data.items[i].firstName;
+      obj.lastName = data.items[i].lastName;
+      obj.age = data.items[i].age;
+      obj.gender = data.items[i].gender;
+      obj.weight = data.items[i].weight;
+      obj.height = data.items[i].height;
+      obj.bloodPressure = data.items[i].bloodPressure;
+      obj.pulseRate = data.items[i].pulseRate;
+      obj.exercisePerWeek = data.items[i].avgHourseOfExercisePerWeek;
+      obj.vacationBalance = data.items[i].vacationBalance;
+      obj.workPerWeek = data.items[i].avgHourseOfWorkPerWeek;
+      obj.bodyTemp = data.items[i].bodyTemperature;
+      obj.respirationRate = data.items[i].respirationRate;
+  
+      this.arrayObj[i] = obj;
+    }
+    console.log(this.arrayObj);
   }
-
+  
+  
+)
+}
 }
