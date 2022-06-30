ed46435756dba4d7d5fbb404a058c8cd093e0eb6
Hamidullahaslan <hamid_omed@yahoo.com>
2022-06-29 22:44:52 -0700

    updating add employee

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import {createVendiaClient} from "@vendia/client";
+
+const client = createVendiaClient({
+
+  apiUrl: `https://z2z10z6138.execute-api.us-west-1.amazonaws.com/graphql/`,
+  websocketUrl: `wss://ssope56ubl.execute-api.us-west-1.amazonaws.com/graphql`,
+  apiKey: `naUSK3QaBR8gPJcjj8N8diBaDNcuBdSe9UgDooCJciD`,
+});
+
+const {entities} = client;
 
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
   styleUrls: ['./add-employee.component.css']
 })
+
 export class AddEmployeeComponent implements OnInit {
   constructor(private router: Router) {
   }
@@ -23,6 +34,29 @@ export class AddEmployeeComponent implements OnInit {
 
   goToDashboard()
   {
-    this.router.navigate(['']);
+    this.async().then();
+    this.router.navigate(['']).then();
+  }
+
+  async async() {
+
+    // @ts-ignore
+    const employeeResponse = await entities.employee.add({
+      age: 14,
+      avgHourseOfExercisePerWeek: 56,
+      avgHourseOfWorkPerWeek: 67,
+      bloodPressure: '78',
+      bodyTemperature: 78,
+      firstName: 'Gloria',
+      gender: 'Woman',
+      height: 1,
+      lastName: 'Leone',
+      pulseRate: 56,
+      respirationRate: 45,
+      vacationBalance: 15,
+      weight: 156,
+    });
+
+    console.log(employeeResponse);
   }
 }
